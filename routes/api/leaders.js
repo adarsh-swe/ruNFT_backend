a7a58d37c52397a2ebf10a815e243efcca1cbd4b
hetp4401 <patelhet32@gmail.com>
2022-01-15 12:23:53 -0500

    leaderboards

--- routes/api/leaders.js
+++ routes/api/leaders.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { body, validationResult } = require("express-validator");
 
 const Token = require("../../models/Token");
 
